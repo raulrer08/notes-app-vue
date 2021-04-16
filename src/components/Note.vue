af9efddfa31c0610b5baec5029fa9ab74d76b7d9
Raul Ramos <cagefightingstfc@yahoo.com>
2021-04-16 10:12:13 -0500

    finished notes app

---
+++ src/components/Note.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-yellow-400 text-yellow-800 rounded text-center p-2 space-y-2">
+    <h3 class="text-2xl font-thin tracking-wide">{{ $props.note.title }}</h3>
+    <p class="text-sm font-medium text-yellow-900">
+      {{ $props.note.content }}
+    </p>
+    <div class="flex justify-end space-x-2">
+      <button @click="editNote(note)" class="hover:text-green-800">
+        <mdi:playlist-edit />
+      </button>
+      <button @click="removeNote($props.note.id)" class="hover:text-red-800">
+        <raphael:trash />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmit } from "vue";
+import { remove, showToggle, noteToEdit } from "~/helpers/useNotes";
+defineProps({
+  note: Object,
+  default: {
+    id: 0,
+    title: "",
+    content: "",
+  },
+});
+const emit = defineEmit(["deleted"]);
+const removeNote = async (id) => {
+  await remove(id);
+  emit("deleted");
+};
+const editNote = (note) => {
+  noteToEdit.value = note;
+  showToggle();
+};
+</script>
---
+++ src/components/Notes.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 container mx-auto"
+  >
+    <Note
+      v-for="(note, index) in notes"
+      :key="index"
+      :note="note"
+      @deleted="refresh"
+    />
+    <NoteAdd />
+  </div>
+  <NotesForm v-if="show" @added="refresh" @saved="refresh" />
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { show, notes, getAll } from "~/helpers/useNotes";
+onMounted(() => refresh());
+const refresh = async () => await getAll();
+</script>
